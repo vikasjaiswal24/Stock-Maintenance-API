e7d330cc61b7b53fd1ac8dcbce30a183fa5bc739
vjaiswal <vjaiswal@coremedia.com>
2019-10-02 16:37:10 +0200

    Implemented HandlerInterceptor

--- stock-maintenance/src/main/java/com/coremedia/WebConfig.java
+++ stock-maintenance/src/main/java/com/coremedia/WebConfig.java
@@ -7,12 +7,28 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
+/**
+ * This annotation is the main artifact used by the Java-based Spring configuration;
+ * it is itself meta-annotated with @Component, which makes the annotated classes standard beans
+ * and as such, also candidates for component-scanning.
+ *
+ * The main purpose of @Configuration classes is to be sources of bean definitions for the Spring IoC Container.
+ */
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Autowired
     RedirectHandlerInterceptor requestHandler;
 
+    /**
+     * To add our interceptors into Spring configuration, we need to override addInterceptors() method
+     * inside WebConfig class that implements WebMvcConfigurer:
+     *
+     * We may achieve the same configuration by editing our XML Spring configuration file like below:
+     * <mvc:interceptors>
+     *     <bean id="loggerInterceptor" class="org.baeldung.web.interceptor.LoggerInterceptor"/>
+     * </mvc:interceptors>
+     */
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(requestHandler);
--- stock-maintenance/src/main/java/com/coremedia/handlerInterceptor/RedirectHandlerInterceptor.java
+++ stock-maintenance/src/main/java/com/coremedia/handlerInterceptor/RedirectHandlerInterceptor.java
@@ -16,6 +16,14 @@ import java.util.List;
 
 import com.coremedia.domain.ProductRequest;
 
+
+/**
+ * These three methods provide flexibility to do all kinds of pre- and post-processing.
+ *
+ * prehandle() – called before the actual handler is executed, but the view is not generated yet
+ * postHandle() – called after the handler is executed
+ * afterCompletion() – called after the complete request has finished and view was generated
+ */
 @Controller("org.springframework.web.servlet.handler.BeanNameUrlHandlerMapping")
 public class RedirectHandlerInterceptor extends HandlerInterceptorAdapter {
 
@@ -50,22 +58,32 @@ public class RedirectHandlerInterceptor extends HandlerInterceptorAdapter {
 
     for (int i=9; i>=0; --i) {
       ProductRequest buildStock = new ProductRequest();
+
       buildStock.setId("ST201"+String.valueOf(i));
-      buildStock.setTimeStamp(new Date(2010+i, 8, 8, 10, 17, 32 ));
+      buildStock.setTimeStamp(new Date(2010+i, 1, 8, 10, 17, 32 ));
       //buildStock.setTimeStamp(new Date("2019-01-12T17:52:15.015Z"));
       buildStock.setProductId(110+i);
       buildStock.setQuantity(10+i);
-
       productList.add(buildStock);
 
       buildStock.setId("ST201"+String.valueOf(i));
-      buildStock.setTimeStamp(new Date(2010+i, 9, 9, 11, 37, 17 ));
+      buildStock.setTimeStamp(new Date(2010+i, 4, 9, 11, 37, 17 ));
       buildStock.setProductId(210+i);
       buildStock.setQuantity(50+i);
+      productList.add(buildStock);
 
+      buildStock.setId("ST201"+String.valueOf(i));
+      buildStock.setTimeStamp(new Date(2010+i, 7, 4, 20, 14, 52 ));
+      buildStock.setProductId(310+i);
+      buildStock.setQuantity(80+i);
+      productList.add(buildStock);
+
+      buildStock.setId("ST201"+String.valueOf(i));
+      buildStock.setTimeStamp(new Date(2010+i, 10, 9, 21, 7, 11 ));
+      buildStock.setProductId(410+i);
+      buildStock.setQuantity(90+i);
       productList.add(buildStock);
     }
-
     return productList;
   }
 
